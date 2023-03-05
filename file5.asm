a55210ed7d0260ce08b31f3561deac92c2171b4b
Yuri Kovach <108182448+YuriBazz@users.noreply.github.com>
2023-03-05 15:15:40 +0500

    Update file5.asm

--- file5.asm
+++ file5.asm
@@ -2,20 +2,26 @@
 .model flat, stdcall
 
 .data
-x1 dw 3 ; 2 байта
-x2 dw 4
-y1 dw 3
-y2 dw 4
-sq dd 0 ; 4 байта
+x1 dw 2000 ; 2 байта
+x2 dw -2000 
+y1 dw 2000
+y2 dw -2000
+
+.data?
+ sq dd ?
 
 .code
 main:
- mov ax, x1
- sub ax, x2
- mov bx, y1
- sub bx, y2
- imul bx
+ movsx eax, x1
+ movsx ebx, x2
+ sub eax, ebx
  mov sq, eax
+ movsx ebx, y1
+ movsx eax, y2
+ sub ebx, eax
+ mov eax, sq
+ imul ebx
+ mov sq, eax
+ 
  ret
 end main
- 
